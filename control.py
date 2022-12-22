f52d59fab0e09929c0ee11357ef455177e118959
last-war <andrey.maybrodskyy@gmail.com>
2022-12-23 00:52:15 +0200

    перевірка телефону та е-маилу

---
+++ control.py
@@ -0,0 +1,48 @@
+import re
+import requests
+
+
+def check_phone(text: str) -> bool:
+    """validation phone
+
+    Args:
+        text (str): text phone for check
+
+    Returns:
+        bool: if match some mask: +380991112233 or 099-111-22-33 or 099-111-2233 or 0991112233
+        or (099)1112233 or (099)111-22-33 or (099)111-2233
+    """
+    result = re.search(
+        r"\d{3}\-\d{3}\-\d{2}\-\d{2}|\d{3}\-\d{3}\-\d{4}|\(\d{3}\)\d{3}\-\d{2}\-\d{2}|\(\d{3}\)\d{3}\-\d{4}|\(\d{3}\)\d{7}|\d{10}|\+\d{12}$", text)
+    return not result is None
+
+
+def check_email(text: str) -> bool:
+    """validation email
+
+    Args:
+        text (str): text email for check
+
+    Returns:
+        bool: if first haven't !#$%^&*() and one @
+        domens from 1-63 len and 1st domen name just letter 1-6 len
+    """
+
+    result = re.search(
+        r"^(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6}$", text)
+    return not result is None
+
+
+def main():
+    # for test
+    list_test = ["nfn@d-f.dd", "SDFSDFS@f989ff.com",
+                 "dooo@com.ua", "test-e@com.uu", "last..war@gmail.com"]
+    for test_str in list_test:
+        print(test_str)
+        print(check_email(test_str))
+
+    pass
+
+
+if __name__ == '__main__':
+    main()
